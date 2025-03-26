f9150910b84615e4620d74d51a1c8c0f472c2925
lh77hl <43295569+lh77hl@users.noreply.github.com>
2025-03-26 15:31:48 +0800

    Add files via upload

---
+++ GLLS_LMAG.m
@@ -0,0 +1,85 @@
+clc,clear,close all
+
+
+clc;
+clear;
+close all;
+addpath('.\utils\differential operators\')
+addpath('.\utils\high-order tensor-SVD Toolbox\')
+
+%% setup parameters
+lambdaL =4;%
+L1=20;%
+
+saveDir1= '.\test_results\T\';
+saveDir2= '.\test_results\B\';
+
+imgpath='.\data\';
+
+imgDir = dir([imgpath '*.bmp']);
+len = length(imgDir);
+tic
+for i=1:len
+    picname=[imgpath  imgDir(i).name];
+    I=imread(picname);%
+    [m,n]=size(I);
+    [~, ~, ch]=size(I);
+    if ch==3
+        I=rgb2gray(I); 
+    end
+    D(:,:,i)=I;
+end
+tenD=double(D);
+[n1,n2,n3]=size(tenD);
+
+n_1=max(n1,n2);%n(1)
+n_2=min(n1,n2);%n(2)
+patch_frames=L1;% temporal slide parameter
+patch_num=n3/patch_frames;
+
+%% constrcut image tensor
+for l=1:patch_num
+    l
+    for i=1:patch_frames
+        temp(:,:,i)=tenD(:,:,patch_frames*(l-1)+i);
+    end           
+        lambda4 =lambdaL / sqrt(max(n_1,n_2)*patch_frames);
+        % dim=size(temp);
+        % A=dim(1);
+        % B=dim(2);
+%% The proposed model
+
+%     [lambda1, lambda2] = structure_tensor_lambda(temp, 3);%
+% %      step 2: calculate corner strength function
+%     cornerStrength = (((lambda1.*lambda2)./(lambda1 + lambda2)));%
+% %      step 3: obtain final weight map
+%     maxValue = (max(lambda1,lambda2));%
+%     priorWeight = mat2gray(cornerStrength .* maxValue);%公式17
+% %      step 4: constrcut patch tensor of weight map
+%     tenW = priorWeight;
+
+opts = [];
+opts.rho = 1.1;
+opts.directions = [1,2,3]; 
+                             
+[tenB,tenT,obj,iter] = draft_GLLS_LMAG(temp, opts);
+
+
+%% recover the target and background image       
+ for i=1:patch_frames
+     tarImg=tenT(:,:,i);
+     backImg=tenB(:,:,i);
+     maxv = max(max(double(I)));
+     tarImg=double(tarImg);
+     backImg=double(backImg);
+
+     T = uint8( mat2gray(tarImg)*maxv );
+     B = uint8( mat2gray(backImg)*maxv );
+
+%% save the results
+imwrite(T, [saveDir1 imgDir(i+patch_frames*(l-1)).name]);     % Save target image 
+imwrite(B, [saveDir2 imgDir(i+patch_frames*(l-1)).name]); % Save background image
+end 
+end
+toc  
+
---
+++ datasets.md
@@ -0,0 +1,19 @@
+We would like to thank researchers for generously sharing their datasets.
+
+#### Dataset 1 [1]：https://www.scidb.cnendetaildataSetId=808025946870251520
+
+​		
+
+#### Dataset 2 [2]：https://zenodo.orgrecords5500576
+
+​		
+
+#### Dataset 3 [3]: https://www.scidb.cnendetaildataSetId=720626420933459968
+
+​		
+
+[1] X. Sun, L. Guo, W. Zhang, Z. Wang, and Q. Yu, “Small aerial target detection for airborne infrared detection systems using LightGBM and trajectory constraints,” IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing, vol. 14, pp. 9959-9973, 2021.
+
+[2] F. Svanström, F. Alonso-Fernandez, and C. Englund. “A dataset for multi-sensor drone detection,” Data in Brief, vol.39, no. 107521, 2021.
+
+[3] B. Hui, Z. Song, H. Fan, P. Zhong, W. Hu, X. Zhang, J. Lin, H. Su, W. Jin, Y. Zhang, and Y. Bai. “A dataset for infrared detection and tracking of dim-small aircraft targets under ground/air background,” China Science Data, vol. 5, no. 3, p. 12, 2020.
